8bf56612a024c9f9adcc69e795defa9b489d1180
gunnmgirl <gunnmgirl@gmail.com>
2024-03-28 04:31:32 +0100

    Sends transaction using Biconomy

--- app/wagmiConfig.ts
+++ app/wagmiConfig.ts
@@ -6,11 +6,22 @@ import {
   arbitrum,
   base,
   sepolia,
+  polygonMumbai,
+  lineaTestnet,
 } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "web3-transaction-manager",
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    sepolia,
+    polygonMumbai,
+    lineaTestnet,
+  ],
   ssr: true,
 });
