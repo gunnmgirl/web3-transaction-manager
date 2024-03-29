13e5d753b128421710a8d1057ca67aa12c290af0
gunnmgirl <gunnmgirl@gmail.com>
2024-03-29 23:36:11 +0100

    Adds footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import { useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import TransactionForm from "app/components/TransactionForm";
 import Switch from "app/components/Switch";
+import Heart from "app/icons/Heart";
 
 const Home = () => {
   const [isGasless, setIsGasless] = useState(true);
   return (
-    <main className="grid min-h-screen grid-rows-[auto,1fr] grid-cols-1 justify-center p-24">
+    <main className="grid min-h-screen grid-rows-[auto,1fr,auto] grid-cols-1 justify-center p-24">
       <div className="grid sm:grid-cols-[1fr,1fr,1fr] grid-rows-[auto,auto] gap-4 justify-center">
         <div className="sm:block hidden" />
         <div className="flex justify-center">
@@ -22,6 +23,10 @@ const Home = () => {
         />
       </div>
       <TransactionForm isGasless={isGasless} />
+      <div className="flex gap-2 items-center justify-self-center">
+        <span>Powered by Biconomy</span>
+        <Heart />
+      </div>
     </main>
   );
 };
