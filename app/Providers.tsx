ca4720acc8af0b5703f163790692ea5acf6f4fc3
gunnmgirl <gunnmgirl@gmail.com>
2024-06-15 22:53:31 +0200

    feat: adds useAA hooks to send sponsored tx

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
+import { BiconomyProvider } from "@biconomy/use-aa";
 import { config } from "app/wagmiConfig";
 import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { BUNDELER_URL } from "./constants";
 import "@rainbow-me/rainbowkit/styles.css";
 
 const queryClient = new QueryClient();
@@ -11,7 +13,18 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={darkTheme()}>{children}</RainbowKitProvider>
+        <RainbowKitProvider theme={darkTheme()}>
+          <BiconomyProvider
+            config={{
+              biconomyPaymasterApiKey:
+                process.env.NEXT_PUBLIC_BICONOMY_PAYMASTER_API_KEY ?? "",
+              bundlerUrl: BUNDELER_URL,
+            }}
+            queryClient={queryClient}
+          >
+            {children}
+          </BiconomyProvider>
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
