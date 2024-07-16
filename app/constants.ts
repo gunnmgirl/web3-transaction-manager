292b3f67a4e548828e4b42fe90c1829cdb8332b7
gunnmgirl <gunnmgirl@gmail.com>
2024-07-16 22:43:17 +0200

    fix: fixes bundler url chain id

--- app/constants.ts
+++ app/constants.ts
@@ -1,2 +1,2 @@
 export const BUNDELER_URL =
-  "https://bundler.biconomy.io/api/v2/80002/cJPK7B3ru.d";
+  "https://bundler.biconomy.io/api/v2/11155111/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44";
